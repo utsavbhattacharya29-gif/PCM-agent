f6829d1bee19404d0924f8107c52f0aa62ca9bff
utsav_bhattacharjee <utsavbhattacharya29@gmail.com>
2026-09-03 19:39:40 +0530

    Update test_symbolic.py

--- tests/test_symbolic.py
+++ tests/test_symbolic.py
@@ -1,1 +1,14 @@
+from app.symbolic.solver import MathSolver
 
+
+solver = MathSolver()
+
+problem = {
+    "operation": "solve_equation",
+    "equation": "2*x + 5 = 15",
+    "variable": "x"
+}
+
+result = solver.solve(problem)
+
+print(result)
