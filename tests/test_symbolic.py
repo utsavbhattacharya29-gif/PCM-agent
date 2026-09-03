c125215d65f349e24f30890be5c6283dcd0093c9
utsav_bhattacharjee <utsavbhattacharya29@gmail.com>
2026-09-03 19:48:56 +0530

    Update test_symbolic.py

--- tests/test_symbolic.py
+++ tests/test_symbolic.py
@@ -3,12 +3,44 @@ from app.symbolic.solver import MathSolver
 
 solver = MathSolver()
 
-problem = {
-    "operation": "solve_equation",
-    "equation": "2*x + 5 = 15",
-    "variable": "x"
-}
 
-result = solver.solve(problem)
+problems = [
 
-print(result)
+    {
+        "operation": "solve_equation",
+        "equation": "2*x + 5 = 15",
+        "variable": "x"
+    },
+
+    {
+        "operation": "simplify",
+        "expression": "(x + 2) + (x + 3)"
+    },
+
+    {
+        "operation": "expand",
+        "expression": "(x + 2)*(x + 3)"
+    },
+
+    {
+        "operation": "factor",
+        "expression": "x**2 + 5*x + 6"
+    },
+
+    {
+        "operation": "differentiate",
+        "expression": "x**3 + 2*x",
+        "variable": "x"
+    },
+
+    {
+        "operation": "integrate",
+        "expression": "3*x**2",
+        "variable": "x"
+    }
+]
+
+
+for problem in problems:
+    result = solver.solve(problem)
+    print(problem["operation"], "->", result)
