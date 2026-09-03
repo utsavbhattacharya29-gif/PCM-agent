953adb068a7901274755d6518830c06b99b545b3
utsav_bhattacharjee <utsavbhattacharya29@gmail.com>
2026-09-03 19:30:19 +0530

    Implement symbolic math functions in SymbolicEngine

--- app/symbolic/engine.py
+++ app/symbolic/engine.py
@@ -1,1 +1,25 @@
+import sympy as sp
 
+
+class SymbolicEngine:
+
+    def solve_equation(self, equation, variable):
+        lhs, rhs = equation.split("=")
+
+        lhs = sp.sympify(lhs)
+        rhs = sp.sympify(rhs)
+
+        symbol = sp.Symbol(variable)
+
+        solution = sp.solve(sp.Eq(lhs, rhs), symbol)
+
+        return solution
+
+    def simplify(self, expression):
+        return sp.simplify(sp.sympify(expression))
+
+    def expand(self, expression):
+        return sp.expand(sp.sympify(expression))
+
+    def factor(self, expression):
+        return sp.factor(sp.sympify(expression))
