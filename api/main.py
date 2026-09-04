23cbb6cb8bcddbd34185d6aaee32244041421403
utsav_bhattacharjee <utsavbhattacharya29@gmail.com>
2026-09-04 12:57:41 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -14,11 +14,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -41,7 +38,5 @@ def root():
 
 @app.post("/solve")
 def solve(request: SolveRequest):
-
     result = router.solve(request.question)
-
     return result
